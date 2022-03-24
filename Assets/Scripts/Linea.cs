adaccef6b226fe4651bd2a94650436d365a290ed
Morion <adrian.martinez@udea.edu.co>
2022-03-24 16:13:35 -0500

    Flechas con curvas

--- Assets/Scripts/Linea.cs
+++ Assets/Scripts/Linea.cs
@@ -8,15 +8,25 @@ public class Linea : MonoBehaviour
     public Transform objetivo;
     public float offset = 0.01f;
     LineRenderer l;
+    public Transform pivote;
+    public int secciones;
     void Start()
     {
         l = GetComponent<LineRenderer>();
+        l.positionCount = secciones;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        l.SetPosition(0, transform.position + transform.up * offset);
-        l.SetPosition(1, objetivo.position + transform.up * offset);
+        for (int i = 0; i < secciones; i++)
+        {
+            transform.LookAt(objetivo);
+            transform.localScale = Vector3.one * (transform.position - objetivo.position).magnitude;
+            l.SetPosition(i, Curva((float)i/(float)secciones) + transform.up*offset);
+        }
+    }
+
+    public Vector3 Curva(float t)
+    {
+        return (Mathf.Pow(1 - t, 2) * transform.position + 2 * (1 - t) * t * pivote.position + Mathf.Pow(t, 2) * objetivo.position);
     }
 }
